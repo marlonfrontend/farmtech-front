3b5c6e39f8f38f2bfeed57981cb8f252e6bd5142
Marlon Felix <marlon.santos.ext@dasa.com.br>
2023-06-03 17:38:00 -0300

    feat: model value input component

--- src/components/core/AppInput.vue
+++ src/components/core/AppInput.vue
@@ -1,11 +1,41 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const emit = defineEmits(['update:modelValue'])
+
+const props = defineProps({
+  icon: String,
+  modelValue: String,
+  placeholder: String
+})
+
+const classInput = computed(() => ({
+  'border-0 border-b border-primary w-full py-2 focus:outline-none': true,
+  'pl-10': props.icon
+}))
+
+const handleInput = (e: Event) => {
+  const target = e.target as HTMLInputElement
+  emit('update:modelValue', target.value)
+}
+
+defineExpose(props)
+</script>
 
 <template>
   <div class="relative">
+    <app-icon
+      v-if="icon"
+      :icon="icon"
+      :size="20"
+      class="text-primary absolute top-1/2 -translate-y-1/2"
+    />
     <input
       type="text"
-      class="border-0 border-b border-primary w-full py-2 focus:outline-none"
-      placeholder="Digite o nome do país"
+      :value="modelValue"
+      @input="handleInput"
+      :class="classInput"
+      :placeholder="placeholder"
     />
   </div>
 </template>
