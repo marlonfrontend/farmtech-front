94e411c5ecc36a69f474f906ee935d599d5b11ca
Marlon Felix <marlon.santos.ext@dasa.com.br>
2023-06-03 19:16:25 -0300

    feat: created skeletom loading component

---
+++ src/components/core/AppSkeleton.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const props = defineProps({
+  width: {
+    type: String,
+    default: '180px'
+  },
+  height: {
+    type: String,
+    default: '20px'
+  }
+})
+
+defineExpose(props)
+</script>
+
+<template>
+  <div
+    class="leading-relaxed mx-auto animate-pulse bg-gray-300"
+    :style="`width: ${width}; height: ${height};`"
+  ></div>
+</template>
