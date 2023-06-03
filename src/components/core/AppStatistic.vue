08fbd1797eeaea8fbf0b04f4ed77295a9f314171
Marlon Felix <marlon.santos.ext@dasa.com.br>
2023-06-03 19:16:02 -0300

    feat: created statistic component

---
+++ src/components/core/AppStatistic.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const props = defineProps({
+  title: String,
+  value: [String, Number],
+  bordered: Boolean
+})
+
+defineExpose(props)
+</script>
+
+<template>
+  <div class="text-center" :class="bordered ? 'border-y py-2 sm:border-x sm:border-y-0' : null">
+    <div data-testid="title" class="font-medium">
+      {{ title }}
+    </div>
+
+    <div v-if="value" class="font-serif text-primary text-xl">
+      <span data-testid="value" v-html="value === null ? '-' : value" />
+    </div>
+  </div>
+</template>
