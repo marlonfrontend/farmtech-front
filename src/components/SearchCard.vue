7778826b6b847f4b48c8c28ef24a2ee7e0a9f333
Marlon Felix <marlon.santos.ext@dasa.com.br>
2023-06-03 19:21:01 -0300

    chore: country list filter and rename search card component

---
+++ src/components/SearchCard.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useCountryStore } from '@/stores'
+
+const countryStore = useCountryStore()
+</script>
+
+<template>
+  <app-card title="Filtrar dados sobre um país">
+    <form class="px-20">
+      <app-input v-model="countryStore.filter" icon="search" placeholder="Digite o nome do país" />
+    </form>
+  </app-card>
+</template>
--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,20 +1,44 @@
 import { defineStore } from 'pinia'
+import type { CountryInfoProps } from '@/components/CountryInfoCard.vue'
 
+type StateProps = {
+  loading: boolean
+  countries: CountryInfoProps[]
+  filter: string
+}
 export const useCountryStore = defineStore('country', {
-  state: () => ({
-    countries: []
+  state: (): StateProps => ({
+    loading: false,
+    countries: [],
+    filter: ''
   }),
+
   actions: {
     fetchCountries() {
+      this.loading = true
       fetch(
         'https://api.apify.com/v2/key-value-stores/tVaYRsPHLjNdNBu7S/records/LATEST?disableRedirect=true'
       )
         .then((response) => response.json())
-        .then((data) => (this.countries = data))
+        .then(
+          (data) =>
+            (this.countries = data.map((item: CountryInfoProps) => {
+              const percentage = ((item.deceased / item.infected) * 100).toFixed(2) + '%'
+              return { ...item, fatality: percentage }
+            }))
+        )
         .catch((error) => console.log(error))
+        .finally(() => (this.loading = false))
     }
   },
+
   getters: {
-    teste: (state) => state.countries
+    filteredCountries: (state) =>
+      state.countries.filter((item) => {
+        if (!state.filter) {
+          return !!item.infected && !!item.deceased
+        }
+        return item.country.toLocaleLowerCase().includes(state.filter.toLocaleLowerCase())
+      })
   }
 })
