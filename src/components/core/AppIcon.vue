dc85f24ada3c04bc52190d223de59961aaddb80b
Marlon Felix <marlon.santos.ext@dasa.com.br>
2023-06-03 11:05:15 -0300

    feat: created icon component

---
+++ src/components/core/AppIcon.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const props = defineProps({
+  icon: String,
+  size: {
+    type: Number,
+    default: 24
+  }
+})
+
+defineExpose(props)
+</script>
+
+<template>
+  <svg viewBox="0 0 24 24" :width="`${size}px`" :height="`${size}px`">
+    <use
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      :xlink:href="`/icons.svg#${icon}`"
+      :href="`/icons.svg#${icon}`"
+    />
+  </svg>
+</template>
